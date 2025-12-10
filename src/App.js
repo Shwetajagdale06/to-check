a0d50b3c65b81e88c4c33bbe41060cb74f0ab491
Shweta <Shwetajagdale06@users.noreply.github.com>
2025-12-10 16:23:24 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign: 'center', marginTop: '80px'}}>
+      <h1>Welcome to React Website!</h1>
+      <p>This page is deployed automatically from GitHub → EC2 🎉</p>
     </div>
   );
 }
