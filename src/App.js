58c326abbb09cda8328c0f83a81922ba0d95419b
Shwetajagdale06 <120297994+Shwetajagdale06@users.noreply.github.com>
2025-12-10 16:34:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div style={{textAlign: 'center', marginTop: '80px'}}>
       <h1>Welcome to React Website!</h1>
-      <p>This page is deployed automatically from GitHub → EC2 🎉</p>
+      <p>This page is deployed automatically from GitHub → EC2 🎉..</p>
     </div>
   );
 }
